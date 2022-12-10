c92e09227ebb0f12e3124414c1fed2651947539b
Abdihamid Moallim <66555990+abmoallim@users.noreply.github.com>
2022-12-10 16:10:11 +0300

    Delete bloodbank_API directory

--- bloodbank_API/src/main/java/uct/so/bloodbank_API/Repositories/bloodTypeRepo.java
+++
@@ -1,9 +0,0 @@
-package uct.so.bloodbank_API.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import uct.so.bloodbank_API.Modals.bloodType;
-
-@Repository
-public interface bloodTypeRepo extends JpaRepository<bloodType,Long> {
-}
